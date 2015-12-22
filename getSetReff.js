1a90f7b9c9e6ccea0c98fec4a49c227cc3724c0b
Andras Sevcsik <sevcsik@gmail.com>
2015-12-22 02:38:22 +0100

    moved functions back to scope

--- getSetReff.js
+++ getSetReff.js
@@ -1,8 +1,6 @@
 (function() {
 
 function rC(k){return(document.cookie.match('(^|; )'+k+'=([^;]*)')||0)[2]}
-function sC(n,v,d) { dd = new Date(); dd.setTime(dd.getTime() + (d*24*60*60*1000)) ;e = typeof d != "undefined" ? ";expires="+dd.toGMTString() : ""; document.cookie = n+"="+v+e+";domain="+_reff[0].setDomain+";path=/"; }
-function gcP(p) { if (document.location.search.indexOf(p) != -1) { return (""+document.location.search.split(p+"=")[1]).split("&")[0]; } else { return "not-set"; }  }
 
 function getSetReff()
 {
@@ -46,6 +44,9 @@ function getSetReff()
     var _reff=[];
     _reff = dataLayer.filter(function (value) {if (value.setDomain)  return value; });
     if (_reff.length === 0) _reff[0]={'setDomain':document.location.hostname};
+
+    function sC(n,v,d) { dd = new Date(); dd.setTime(dd.getTime() + (d*24*60*60*1000)) ;e = typeof d != "undefined" ? ";expires="+dd.toGMTString() : ""; document.cookie = n+"="+v+e+";domain="+_reff[0].setDomain+";path=/"; }
+    function gcP(p) { if (document.location.search.indexOf(p) != -1) { return (""+document.location.search.split(p+"=")[1]).split("&")[0]; } else { return "not-set"; }  }
     
     //every pageview
     var __asc = (typeof rC("__sreff") != "undefined" ? rC("__sreff") : "");
@@ -133,7 +134,7 @@ function parse() {
         } else {
             // catch-all for now
             return e;
-        }               
+        }
     });
 }
 
